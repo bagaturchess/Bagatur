ab935c74871f8ee8561a20ad8df97ec3bd5c19ec
bagaturchess <k_topchiyski@yahoo.com>
2016-06-11 20:17:30 +0300

    Distro build updates

--- Sources/Search/src/bagaturchess/search/impl/rootsearch/parallel/MTDParallelSearch.java
+++ Sources/Search/src/bagaturchess/search/impl/rootsearch/parallel/MTDParallelSearch.java
@@ -155,7 +155,7 @@ public class MTDParallelSearch extends RootSearch_BaseImpl {
 					
 					long start_time = System.currentTimeMillis();
 					
-					SearchersInfo searchersInfo = new SearchersInfo(startIteration, 0.5d);
+					SearchersInfo searchersInfo = new SearchersInfo(startIteration, 0.001d);
 					
 					boolean allSearchersReady = false;
 					//boolean hasSendAtLest1Info = false;
