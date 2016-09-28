c9b75f4d7965ad8a89237af85c2920fa8ecf8e65
bagaturchess <k_topchiyski@yahoo.com>
2016-09-28 13:10:29 +0300

    Minor change

--- Sources/Bitboard/src/bagaturchess/bitboard/api/IBoardConfig.java
+++ Sources/Bitboard/src/bagaturchess/bitboard/api/IBoardConfig.java
@@ -27,42 +27,6 @@ public interface IBoardConfig {
 	
 	public boolean getFieldsStatesSupport();
 	
-	public double getWeight_PST_PAWN_O();
-	public double getWeight_PST_PAWN_E();
-	
-	public double getWeight_PST_KING_O();
-	public double getWeight_PST_KING_E();
-	
-	public double getWeight_PST_KNIGHT_O();
-	public double getWeight_PST_KNIGHT_E();
-	
-	public double getWeight_PST_BISHOP_O();
-	public double getWeight_PST_BISHOP_E();
-	
-	public double getWeight_PST_ROOK_O();
-	public double getWeight_PST_ROOK_E();
-	
-	public double getWeight_PST_QUEEN_O();
-	public double getWeight_PST_QUEEN_E();
-	
-	public double[] getPST_PAWN_O();
-	public double[] getPST_PAWN_E();
-	
-	public double[] getPST_KING_O();
-	public double[] getPST_KING_E();
-	
-	public double[] getPST_KNIGHT_O();
-	public double[] getPST_KNIGHT_E();
-	
-	public double[] getPST_BISHOP_O();
-	public double[] getPST_BISHOP_E();
-	
-	public double[] getPST_ROOK_O();
-	public double[] getPST_ROOK_E();
-	
-	public double[] getPST_QUEEN_O();
-	public double[] getPST_QUEEN_E();
-	
 	public double getMaterial_PAWN_O();
 	public double getMaterial_PAWN_E();
 	
@@ -83,4 +47,42 @@ public interface IBoardConfig {
 	
 	public double getMaterial_BARIER_NOPAWNS_O();
 	public double getMaterial_BARIER_NOPAWNS_E();
+	
+	
+	public double[] getPST_PAWN_O();
+	public double[] getPST_PAWN_E();
+	
+	public double[] getPST_KING_O();
+	public double[] getPST_KING_E();
+	
+	public double[] getPST_KNIGHT_O();
+	public double[] getPST_KNIGHT_E();
+	
+	public double[] getPST_BISHOP_O();
+	public double[] getPST_BISHOP_E();
+	
+	public double[] getPST_ROOK_O();
+	public double[] getPST_ROOK_E();
+	
+	public double[] getPST_QUEEN_O();
+	public double[] getPST_QUEEN_E();
+	
+	
+	public double getWeight_PST_PAWN_O();
+	public double getWeight_PST_PAWN_E();
+	
+	public double getWeight_PST_KING_O();
+	public double getWeight_PST_KING_E();
+	
+	public double getWeight_PST_KNIGHT_O();
+	public double getWeight_PST_KNIGHT_E();
+	
+	public double getWeight_PST_BISHOP_O();
+	public double getWeight_PST_BISHOP_E();
+	
+	public double getWeight_PST_ROOK_O();
+	public double getWeight_PST_ROOK_E();
+	
+	public double getWeight_PST_QUEEN_O();
+	public double getWeight_PST_QUEEN_E();
 }
