4db5791ed7e492e37f6ea5ef33e9552cf98f2986
bagaturchess <k_topchiyski@yahoo.com>
2017-04-01 17:40:03 +0300

    remove unused import

--- Sources/LearningTest/src/bagaturchess/learning/goldmiddle/run/LearningTraverser.java
+++ Sources/LearningTest/src/bagaturchess/learning/goldmiddle/run/LearningTraverser.java
@@ -2,7 +2,6 @@ package bagaturchess.learning.goldmiddle.run;
 
 
 import bagaturchess.learning.goldmiddle.api.ILearningInput;
-import bagaturchess.learning.goldmiddle.impl.cfg.allfeatures.ALL_BoardConfig;
 import bagaturchess.learning.goldmiddle.impl.cfg.allfeatures.ALL_LearningInputImpl;
 import bagaturchess.learning.goldmiddle.impl.cfg.bagatur.Bagatur_LearningInputImpl;
 import bagaturchess.learning.goldmiddle.impl.visitors.LearningVisitorImpl;
@@ -21,8 +20,8 @@ public class LearningTraverser {
 			//String filePath = "./Houdini.15a.short.cg";
 			//String filePath = "./Houdini.15a.cg";
 			//String filePath = "./Arasan13.1.cg";
-			String filePath = "stockfish-7.cg";
-			//String filePath = "glaurung-2.2.cg";
+			//String filePath = "stockfish-7.cg";
+			String filePath = "glaurung-2.2.cg";
 			
 			PositionsVisitor learning = new LearningVisitorImpl();
 			
