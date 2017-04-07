3083d43f2d4a100eb09753b08dc064b303c21446
bagaturchess <k_topchiyski@yahoo.com>
2017-04-07 15:20:22 +0300

    Cleanup of IBitBoard 2

--- Sources/Bitboard/src/bagaturchess/bitboard/api/IBitBoard.java
+++ Sources/Bitboard/src/bagaturchess/bitboard/api/IBitBoard.java
@@ -22,124 +22,53 @@
  */
 package bagaturchess.bitboard.api;
 
-import bagaturchess.bitboard.impl.eval.pawns.model.PawnsModelEval;
-
-//import bagaturchess.bitboagetrd.BackupInfo;
-//import bagaturchess.bitboard.BoardStat;
-//import bagaturchess.bitboard.eval.BaseEvaluation;
 
 public interface IBitBoard extends IBoard {
 	
 	
-	public int[] getOpeningMoves();
-	
 	public void mark();
 	public void reset();
-	
-	public void test(int colour);
-
-	public void setPawnsCache(PawnsEvalCache pawnsCache);
-	public boolean hasMinorOrMajorPieces(int colour);
-	
 	public void revert();
 	
-	//public BackupInfo[] getBackups();
-	
-	public int getLastCaptrueFieldID();
-	
-	/*public void clearInCheckCounters();
-	public int getWhiteInCheckCounts();
-	public int getBlackInCheckCounts();
-
-	public void clearKingMovesCounters();
-	public int getWhiteKingMovesCounts();
-	public int getBlackKingMovesCounts();
-	
-	public int getBlackQueensMovesCounts();
-	public int getWhiteQueensMovesCounts();
-	public void clearQueenMovesCounters();*/
-	
-	public IPlayerAttacks getPlayerAttacks(int colour);
-	public IFieldsAttacks getFieldsAttacks();
-	
-	//public BoardStat getStatistics();
-	public IGameStatus getStatus();
+	public void setPawnsCache(PawnsEvalCache pawnsCache);
 	
 	/**
 	 * Base engine's methods  
 	 */
-	public int genAllMoves_ByFigureID(int fieldID, long excludedToFields,
-			final IInternalMoveList list);
+	public int genAllMoves_ByFigureID(int fieldID, long excludedToFields, final IInternalMoveList list);
 	public int genAllMoves(final IInternalMoveList list, long excludedToFieldsBoard);
-	public int genAllMoves(final IInternalMoveList list, boolean checkKeepersAware);
-	public int genMinorMoves(final IInternalMoveList list);
-	
-	public int genNonCaptureNonPromotionMoves(final IInternalMoveList list);
-	public int genPromotions(final IInternalMoveList list);
-	public int gen2MovesPromotions(final IInternalMoveList list);
-	public int genDirectCheckMoves(final IInternalMoveList list);
-	public int genHiddenCheckMoves(final IInternalMoveList list);
-	public int genAllCheckMoves(final IInternalMoveList list);
-	public int genCapturePromotionCheckMoves(final IInternalMoveList list);
 	public int genKingEscapes(final IInternalMoveList list);
 	
-	public boolean hasMove();
-	public boolean hasMoveInNonCheck();
-	public boolean hasMoveInCheck();
-	public boolean hasCapturePromotionCheck();
-	public boolean hasSingleMove();
-	public boolean hasPromotions();
-	public boolean has2MovePromotions();
-	public boolean hasChecks(int colour);
-	
-
 	public int getCastlingType(int colour);
-
 	public boolean hasRightsToKingCastle(int colour);
 	public boolean hasRightsToQueenCastle(int colour);
+	
+	public boolean hasMoveInNonCheck();
+	public boolean hasMoveInCheck();
+	public boolean hasSingleMove();
 	
 	public boolean isInCheck();
 	public boolean isInCheck(int colour);
 	public boolean isCheckMove(int move);
-	public boolean isDirectCheckMove(int move);
-	public int getChecksCount(int colour);
 	
 	public boolean isPossible(int move);
-	//public boolean opensCheck(long[] move);
 	
 	/**
 	 * Game related methods
 	 */
 	public int getPlayedMovesCount();
-	public int getPlayedMovesCount_Total();
 	public int[] getPlayedMoves();
 	public int getLastMove();
-	public int getLastLastMove();
-	
-	
-	public int getStateRepetition(long hashkey);
-	
-	
-	/**
-	 * Hash keys
-	 */
-	public long getHashKeyAfterMove(final int move);
-	public long getPawnHashKeyAfterMove(final int move);
+	public IGameStatus getStatus();
 	
 	/**
 	 * Birboards
 	 */
-	public long getFigureBitboardByID(int figureID);
 	public long getFreeBitboard();
-	
-	public int getFieldID(int figureID);
-	
-	public IMoveIterator iterator(int iteratorFactoryHandler);
-	//public IBitBoard clone();
 	
 	public boolean getAttacksSupport();
 	public boolean getFieldsStateSupport();
 	public void setAttacksSupport(boolean attacksSupport, boolean fieldsStateSupport);
-	
-	public int getGamePhase();
+	public IPlayerAttacks getPlayerAttacks(int colour);
+	public IFieldsAttacks getFieldsAttacks();
 }
