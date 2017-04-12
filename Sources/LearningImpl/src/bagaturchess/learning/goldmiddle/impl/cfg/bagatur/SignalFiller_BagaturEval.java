8dfd744db4e82e32dd3d1e94287d0ecd9c36034a
bagaturchess <k_topchiyski@yahoo.com>
2017-04-12 16:25:09 +0300

    Fix for Unstoppable Passers in SignalFiller

--- Sources/LearningImpl/src/bagaturchess/learning/goldmiddle/impl/cfg/bagatur/SignalFiller_BagaturEval.java
+++ Sources/LearningImpl/src/bagaturchess/learning/goldmiddle/impl/cfg/bagatur/SignalFiller_BagaturEval.java
@@ -445,13 +445,13 @@ public class SignalFiller_BagaturEval implements FeaturesConstants, FeatureWeigh
 		int PAWNS_PASSED_UNSTOPPABLE = 100 + baseEval.getMaterialRook();
 		
 		int unstoppablePasser = bitboard.getUnstoppablePasser();
-		if (unstoppablePasser > 0) {
-			evalInfo.eval_PawnsUnstoppable_o += PAWNS_PASSED_UNSTOPPABLE;
-			evalInfo.eval_PawnsUnstoppable_e += PAWNS_PASSED_UNSTOPPABLE;
-		} else if (unstoppablePasser < 0) {
-			evalInfo.eval_PawnsUnstoppable_o -= PAWNS_PASSED_UNSTOPPABLE;
-			evalInfo.eval_PawnsUnstoppable_e -= PAWNS_PASSED_UNSTOPPABLE;
-		}
+		//if (unstoppablePasser > 0) {
+			evalInfo.eval_PawnsUnstoppable_o += unstoppablePasser * PAWNS_PASSED_UNSTOPPABLE;
+			evalInfo.eval_PawnsUnstoppable_e += unstoppablePasser * PAWNS_PASSED_UNSTOPPABLE;
+		//} else if (unstoppablePasser < 0) {
+		//	evalInfo.eval_PawnsUnstoppable_o -= PAWNS_PASSED_UNSTOPPABLE;
+		//	evalInfo.eval_PawnsUnstoppable_e -= PAWNS_PASSED_UNSTOPPABLE;
+		//}
 		
 		bitboard.getPawnsCache().unlock();
 	}
