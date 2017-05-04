97552efcb26521ef1462546f40488b6ad103519b
bagaturchess <k_topchiyski@yahoo.com>
2017-05-04 10:38:45 +0300

    New eval updated weights

--- Sources/LearningImpl/src/bagaturchess/learning/goldmiddle/impl/cfg/bagatur_allfeatures/filler/Bagatur_ALL_BoardConfigImpl.java
+++ Sources/LearningImpl/src/bagaturchess/learning/goldmiddle/impl/cfg/bagatur_allfeatures/filler/Bagatur_ALL_BoardConfigImpl.java
@@ -8,20 +8,20 @@ import bagaturchess.bitboard.common.Utils;
 public class Bagatur_ALL_BoardConfigImpl implements IBoardConfig {
 	
 	
-	private double MATERIAL_PAWN_O = 80;
-	private double MATERIAL_PAWN_E = 100;
-		
-	private double MATERIAL_KNIGHT_O = 246;
-	private double MATERIAL_KNIGHT_E = 352;
-	
-	private double MATERIAL_BISHOP_O = 262;
-	private double MATERIAL_BISHOP_E = 366;
-	
-	private double MATERIAL_ROOK_O = 411;
-	private double MATERIAL_ROOK_E = 570;
-	
-	private double MATERIAL_QUEEN_O = 875;
-	private double MATERIAL_QUEEN_E = 1230;
+	public static final double MATERIAL_PAWN_O	=	78.45480512573172;
+	public static final double MATERIAL_PAWN_E	=	92.12632098602359;
+
+	public static final double MATERIAL_KNIGHT_O	=	316.5706589514874;
+	public static final double MATERIAL_KNIGHT_E	=	266.8941557014653;
+
+	public static final double MATERIAL_BISHOP_O	=	340.7503734031214;
+	public static final double MATERIAL_BISHOP_E	=	291.356800307687;
+
+	public static final double MATERIAL_ROOK_O	=	478.75073472754553;
+	public static final double MATERIAL_ROOK_E	=	527.9069447141043;
+
+	public static final double MATERIAL_QUEEN_O	=	1046.5317944835904;
+	public static final double MATERIAL_QUEEN_E	=	985.7115100377412;
 	
 	private double MATERIAL_KING_O = 0;
 	private double MATERIAL_KING_E = 0;
