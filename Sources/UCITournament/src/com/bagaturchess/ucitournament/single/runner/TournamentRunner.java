8ce93e0ada8e0e53da1236052ecaf2b1c4038d8e
Topchiyski <krasimir.topchiyski@sap.com>
2019-04-12 16:31:10 +0300

    Extract constants on class level

--- Sources/UCITournament/src/com/bagaturchess/ucitournament/single/runner/TournamentRunner.java
+++ Sources/UCITournament/src/com/bagaturchess/ucitournament/single/runner/TournamentRunner.java
@@ -44,26 +44,27 @@ import com.bagaturchess.ucitournament.single.schedule.TournamentSchedule_OneRoun
 public class TournamentRunner {
 	
 	
+	private static final EngineProcess engine_workspace = new EngineProcess_BagaturImpl_WorkspaceImpl("Bagatur WS", "");
+	
+	private static final EngineProcess engine_dev 		= new EngineProcess("Bagatur DEV1", "C:\\Users\\i027638\\OneDrive - SAP SE\\DATA\\OWN\\chess\\SOFTWARE\\ARENA\\arena_3.5.1\\Engines\\BagaturEngine_DEV\\Bagatur_64_1_core.exe",
+															new String [0],
+															"C:\\Users\\i027638\\OneDrive - SAP SE\\DATA\\OWN\\chess\\SOFTWARE\\ARENA\\arena_3.5.1\\Engines\\BagaturEngine_DEV\\");
+	
+	private static final EngineProcess engine_17 		= new EngineProcess("Bagatur 1.7", "C:\\Users\\i027638\\OneDrive - SAP SE\\DATA\\OWN\\chess\\SOFTWARE\\ARENA\\arena_3.5.1\\Engines\\BagaturEngine.1.7\\Bagatur_64_1_core.exe",
+															new String [0],
+															"C:\\Users\\i027638\\OneDrive - SAP SE\\DATA\\OWN\\chess\\SOFTWARE\\ARENA\\arena_3.5.1\\Engines\\BagaturEngine.1.7\\");
+
+	
+	private static final EngineProcess komodo 			=  new EngineProcess("C:\\Users\\i027638\\OneDrive - SAP SE\\DATA\\OWN\\chess\\software\\ARENA\\arena_3.5.1\\Engines\\Komodo9\\Windows\\komodo-9.02-64bit.exe",
+															new String [0],
+															"C:\\Users\\i027638\\OneDrive - SAP SE\\DATA\\OWN\\chess\\software\\ARENA\\arena_3.5.1\\Engines\\Komodo9\\Windows\\");
+
+	
 	public static void main(String[] args) {
 		
 		
-		//EngineProcess engine1 = new EngineProcess_BagaturImpl_WorkspaceImpl("Bagatur Workspace1", "");
-		EngineProcess engine2 = new EngineProcess_BagaturImpl_WorkspaceImpl("Bagatur WS2", "");
-		
-		EngineProcess engine1 = new EngineProcess("Bagatur DEV1", "C:\\Users\\i027638\\OneDrive - SAP SE\\DATA\\OWN\\chess\\SOFTWARE\\ARENA\\arena_3.5.1\\Engines\\BagaturEngine_DEV\\Bagatur_64_1_core.exe",
-				new String [0],
-				"C:\\Users\\i027638\\OneDrive - SAP SE\\DATA\\OWN\\chess\\SOFTWARE\\ARENA\\arena_3.5.1\\Engines\\BagaturEngine_DEV\\");
-		
-		
-		/*EngineProcess engine2 = new EngineProcess("Bagatur 1.7", "C:\\Users\\i027638\\OneDrive - SAP SE\\DATA\\OWN\\chess\\SOFTWARE\\ARENA\\arena_3.5.1\\Engines\\BagaturEngine.1.7\\Bagatur_64_1_core.exe",
-				new String [0],
-				"C:\\Users\\i027638\\OneDrive - SAP SE\\DATA\\OWN\\chess\\SOFTWARE\\ARENA\\arena_3.5.1\\Engines\\BagaturEngine.1.7\\");
-		*/
-		
-		/*EngineProcess engine2 = new EngineProcess("C:\\Users\\i027638\\OneDrive - SAP SE\\DATA\\OWN\\chess\\software\\ARENA\\arena_3.5.1\\Engines\\Komodo9\\Windows\\komodo-9.02-64bit.exe",
-				new String [0],
-				"C:\\Users\\i027638\\OneDrive - SAP SE\\DATA\\OWN\\chess\\software\\ARENA\\arena_3.5.1\\Engines\\Komodo9\\Windows\\");
-		 */
+		EngineProcess engine1 = engine_workspace;
+		EngineProcess engine2 = engine_17;
 		
 		
 		EngineProcess[] engines = new EngineProcess[] {engine1, engine2};
@@ -84,10 +85,10 @@ public class TournamentRunner {
 			engine1.setOptions(options);
 			engine2.setOptions(options);
 			
-			ITournamentSchedule schedule = new TournamentSchedule_2Engines(engines, 1000000);
+			ITournamentSchedule schedule = new TournamentSchedule_2Engines(engines, 100000);
 			//ITournamentSchedule schedule = new TournamentSchedule_EvenScores(engines);
 			
-			MatchRunner matchRunner = new MatchRunner_TimePerMove(20);
+			MatchRunner matchRunner = new MatchRunner_TimePerMove(500);
 			//MatchRunner matchRunner = new MatchRunner_FixedDepth(5);
 			//MatchRunner matchRunner = new MatchRunner_FixedNodes(5000);
 			//MatchRunner matchRunner = new MatchRunner_TimeAndInc(60 * 1000, 60 * 1000, 1 * 1000, 1 * 1000);
