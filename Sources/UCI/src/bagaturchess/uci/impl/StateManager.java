3baa988c553d672a1539819699ba78c1a69fcd07
Krasimir Topchiyski <k_topchiyski@yahoo.com>
2019-12-11 11:25:23 +0200

    Remove imports

--- Sources/UCI/src/bagaturchess/uci/impl/StateManager.java
+++ Sources/UCI/src/bagaturchess/uci/impl/StateManager.java
@@ -25,14 +25,10 @@ package bagaturchess.uci.impl;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
 import bagaturchess.bitboard.api.BoardUtils;
 import bagaturchess.bitboard.api.IBitBoard;
-import bagaturchess.bitboard.api.IBoardConfig;
-import bagaturchess.bitboard.impl.movegen.MoveInt;
-import bagaturchess.bitboard.impl1.internal.MoveWrapper;
 import bagaturchess.uci.api.BestMoveSender;
 import bagaturchess.uci.api.IChannel;
 import bagaturchess.uci.api.IUCIConfig;
