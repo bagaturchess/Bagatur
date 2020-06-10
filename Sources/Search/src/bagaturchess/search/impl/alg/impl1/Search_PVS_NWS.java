21943bc24f6eaf272d12eed021628f16c424cd71
Krasimir Topchiyski <k_topchiyski@yahoo.com>
2020-06-10 08:44:38 +0300

    Add again historyAVGScores in LMR

--- Sources/Search/src/bagaturchess/search/impl/alg/impl1/Search_PVS_NWS.java
+++ Sources/Search/src/bagaturchess/search/impl/alg/impl1/Search_PVS_NWS.java
@@ -510,12 +510,12 @@ public class Search_PVS_NWS extends SearchImpl {
 					
 					reduction = LMR_TABLE[Math.min(depth, 63)][Math.min(movesPerformed, 63)];
 					
-					/*if (moveGen.getScore() > historyAVGScores.getEntropy()) {
+					if (moveGen.getScore() > historyAVGScores.getEntropy()) {
 						reduction -= 1;
 						if (moveGen.getScore() > 2 * historyAVGScores.getEntropy()) {
 							reduction -= 1;
 						}
-					}*/
+					}
 					
 					if (move == killer1Move || move == killer2Move || move == counterMove) {
 						reduction -= 1;
