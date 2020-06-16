201908ed8c54172e9d41884a4bf1c8b8230b8b3a
Krasimir Topchiyski <k_topchiyski@yahoo.com>
2020-06-16 19:56:35 +0300

    Call null move on depth == 2

--- Sources/Search/src/bagaturchess/search/impl/alg/impl1/Search_PVS_NWS.java
+++ Sources/Search/src/bagaturchess/search/impl/alg/impl1/Search_PVS_NWS.java
@@ -337,7 +337,7 @@ public class Search_PVS_NWS extends SearchImpl {
 			}
 
 			
-			if (EngineConstants.ENABLE_NULL_MOVE && depth > 2) {
+			if (EngineConstants.ENABLE_NULL_MOVE && depth >= 2) {
 				if (eval >= beta && MaterialUtil.hasNonPawnPieces(cb.materialKey, cb.colorToMove)) {
 					cb.doNullMove();
 					final int reduction = depth / 4 + 3 + Math.min((eval - beta) / 80, 3);
