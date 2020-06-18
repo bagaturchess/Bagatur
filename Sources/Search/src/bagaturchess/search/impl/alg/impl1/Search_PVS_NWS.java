42c8ee29d1b6262aa687a6538017a4ad819bd760
Krasimir Topchiyski <k_topchiyski@yahoo.com>
2020-06-18 10:00:49 +0300

    Small change

--- Sources/Search/src/bagaturchess/search/impl/alg/impl1/Search_PVS_NWS.java
+++ Sources/Search/src/bagaturchess/search/impl/alg/impl1/Search_PVS_NWS.java
@@ -376,7 +376,8 @@ public class Search_PVS_NWS extends SearchImpl {
 					if (ttMove == 0) {
 						if (EngineConstants.ENABLE_IID && depth > ply + 1) {
 							if (MaterialUtil.containsMajorPieces(cb.materialKey)) {
-								calculateBestMove(mediator, info, pvman, evaluator, cb, moveGen, ply, depth - EngineConstants.IID_REDUCTION - 1, alpha, beta, true);
+								int reduction = EngineConstants.IID_REDUCTION + 1;
+								calculateBestMove(mediator, info, pvman, evaluator, cb, moveGen, ply, depth - reduction, alpha, beta, true);
 								env.getTPT().get(cb.zobristKey, tt_entries_per_ply[ply]);
 								if (!tt_entries_per_ply[ply].isEmpty() && cb.isValidMove(tt_entries_per_ply[ply].getBestMove())) {
 									ttMove = tt_entries_per_ply[ply].getBestMove();
