ddfd4a5c579965f3cd879140c955683124a72f18
Topchiyski <krasimir.topchiyski@sap.com>
2020-07-03 12:18:11 +0300

    Move gen improvements and changedMinor call for each move on ply == 0

--- Sources/Search/src/bagaturchess/search/impl/alg/impl1/Search_PVS_NWS.java
+++ Sources/Search/src/bagaturchess/search/impl/alg/impl1/Search_PVS_NWS.java
@@ -402,26 +402,20 @@ public class Search_PVS_NWS extends SearchImpl {
 					killer1Move = moveGen.getKiller1(ply);
 					if (killer1Move != 0 && killer1Move != ttMove && cb.isValidMove(killer1Move)) {
 						moveGen.addMove(killer1Move);
-						break;
-					} else {
-						phase++;
 					}
+					break;
 				case PHASE_KILLER_2:
 					killer2Move = moveGen.getKiller2(ply);
 					if (killer2Move != 0 && killer2Move != ttMove && cb.isValidMove(killer2Move)) {
 						moveGen.addMove(killer2Move);
-						break;
-					} else {
-						phase++;
 					}
+					break;
 				case PHASE_COUNTER:
 					counterMove = moveGen.getCounter(cb.colorToMove, parentMove);
 					if (counterMove != 0 && counterMove != ttMove && counterMove != killer1Move && counterMove != killer2Move && cb.isValidMove(counterMove)) {
 						moveGen.addMove(counterMove);
-						break;
-					} else {
-						phase++;
 					}
+					break;
 				case PHASE_QUIET:
 					moveGen.generateMoves(cb);
 					moveGen.setHHScores(cb.colorToMove, parentMove);
@@ -443,6 +437,7 @@ public class Search_PVS_NWS extends SearchImpl {
 				if (ply == 0) {
 					info.setCurrentMove(move);
 					info.setCurrentMoveNumber((movesPerformed + 1));
+					mediator.changedMinor(info);
 				}
 				
 				if (info.getSearchedNodes() >= lastSentMinorInfo_nodesCount + 50000 ) { //Check time on each 50 000 nodes
