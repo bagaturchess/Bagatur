6b4d95dd2ca0f5b2488312accee21648415d8eac
Topchiyski <krasimir.topchiyski@sap.com>
2020-07-31 11:12:51 +0300

    Use TT scores as correction of eval in qsearch

--- Sources/Search/src/bagaturchess/search/impl/alg/impl1/Search_PVS_NWS.java
+++ Sources/Search/src/bagaturchess/search/impl/alg/impl1/Search_PVS_NWS.java
@@ -640,6 +640,16 @@ public class Search_PVS_NWS extends SearchImpl {
 		}
 		
 		int eval = eval(evaluator, ply, alpha, beta, isPv);
+		
+		if (EngineConstants.USE_TT_SCORE_AS_EVAL && !tt_entries_per_ply[ply].isEmpty()) {
+			if (tt_entries_per_ply[ply].getFlag() == ITTEntry.FLAG_EXACT
+					|| (tt_entries_per_ply[ply].getFlag() == ITTEntry.FLAG_UPPER && tt_entries_per_ply[ply].getEval() < eval)
+					|| (tt_entries_per_ply[ply].getFlag() == ITTEntry.FLAG_LOWER && tt_entries_per_ply[ply].getEval() > eval)
+				) {
+				eval = tt_entries_per_ply[ply].getEval();
+			}
+		}
+		
 		if (eval >= beta) {
 			return eval;
 		}
@@ -757,7 +767,7 @@ public class Search_PVS_NWS extends SearchImpl {
 			result.bestmove = 0;
 			return true;
 		}
-
+		
 		if (info.getSelDepth() < ply) {
 			info.setSelDepth(ply);
 		}
