3b3ae6e4feb74fff9892e9b2080b2774e8cc59ed
Topchiyski <krasimir.topchiyski@sap.com>
2020-10-15 16:44:49 +0300

    Small refactoring: extract ttMove in advance

--- Sources/Search/src/bagaturchess/search/impl/alg/impl1/Search_PVS_NWS.java
+++ Sources/Search/src/bagaturchess/search/impl/alg/impl1/Search_PVS_NWS.java
@@ -667,8 +667,10 @@ public class Search_PVS_NWS extends SearchImpl {
 			info.setSelDepth(ply);
 		}
 		
+		int ttMove = 0;
 		env.getTPT().get(cb.zobristKey, tt_entries_per_ply[ply]);
 		if (!tt_entries_per_ply[ply].isEmpty() && cb.isValidMove(tt_entries_per_ply[ply].getBestMove())) {
+			ttMove = tt_entries_per_ply[ply].getBestMove();
 			if (tt_entries_per_ply[ply].getFlag() == ITTEntry.FLAG_EXACT) {
 				return tt_entries_per_ply[ply].getEval();
 			} else {
@@ -703,12 +705,9 @@ public class Search_PVS_NWS extends SearchImpl {
 		while (phase <= PHASE_ATTACKING_GOOD) {
 			switch (phase) {
 				case PHASE_TT:
-					if (!tt_entries_per_ply[ply].isEmpty()) {
-						int ttMove = tt_entries_per_ply[ply].getBestMove();
-						if (cb.isValidMove(ttMove)) {
-							if (env.getBitboard().getMoveOps().isCaptureOrPromotion(ttMove)) {
-								moveGen.addMove(ttMove);
-							}
+					if (ttMove != 0) {
+						if (env.getBitboard().getMoveOps().isCaptureOrPromotion(ttMove)) {
+							moveGen.addMove(ttMove);
 						}
 					}
 					break;
