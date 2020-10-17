b08885900aa14ecdaad14b4dcaf715ec1897c9d2
Topchiyski <krasimir.topchiyski@sap.com>
2020-10-17 16:30:24 +0300

    Enable SEE pruning on depth == 7

--- Sources/Search/src/bagaturchess/search/impl/alg/impl1/Search_PVS_NWS.java
+++ Sources/Search/src/bagaturchess/search/impl/alg/impl1/Search_PVS_NWS.java
@@ -538,7 +538,7 @@ public class Search_PVS_NWS extends SearchImpl {
 								}
 							}
 						}
-					} else if (EngineConstants.ENABLE_SEE_PRUNING && depth <= 6 && phase == PHASE_ATTACKING_BAD
+					} else if (EngineConstants.ENABLE_SEE_PRUNING && depth <= 7 && phase == PHASE_ATTACKING_BAD
 							&& SEEUtil.getSeeCaptureScore(cb, move) < -20 * depth * depth) {
 						continue;
 					}
