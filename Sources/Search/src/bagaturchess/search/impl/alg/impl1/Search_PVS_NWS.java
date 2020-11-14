49b5a47134659212bf711bfd452d68dec11005cb
Topchiyski <krasimir.topchiyski@sap.com>
2020-11-14 14:02:49 +0200

    Remove comments

--- Sources/Search/src/bagaturchess/search/impl/alg/impl1/Search_PVS_NWS.java
+++ Sources/Search/src/bagaturchess/search/impl/alg/impl1/Search_PVS_NWS.java
@@ -904,14 +904,12 @@ public class Search_PVS_NWS extends SearchImpl {
 		int eval = eval(evaluator, ply, alpha, beta, isPv);
 		
 		if (EngineConstants.USE_TT_SCORE_AS_EVAL) {
-			//if (tpt_depth >= 1) {
-				if (ttFlag == ITTEntry.FLAG_EXACT
-						|| (ttFlag == ITTEntry.FLAG_UPPER && ttValue < eval)
-						|| (ttFlag == ITTEntry.FLAG_LOWER && ttValue > eval)
-					) {
-					eval = ttValue;
-				}
-			//}
+			if (ttFlag == ITTEntry.FLAG_EXACT
+					|| (ttFlag == ITTEntry.FLAG_UPPER && ttValue < eval)
+					|| (ttFlag == ITTEntry.FLAG_LOWER && ttValue > eval)
+				) {
+				eval = ttValue;
+			}
 		}
 		
 		if (eval >= beta) {
