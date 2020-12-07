c88c13d5bdbad03dee1c94e62bebd426d2a0506e
Topchiyski <krasimir.topchiyski@sap.com>
2020-12-07 15:52:40 +0200

    Swithc to SF12 games

--- Sources/LearningImpl/src/bagaturchess/learning/goldmiddle/run/LearningTraverser.java
+++ Sources/LearningImpl/src/bagaturchess/learning/goldmiddle/run/LearningTraverser.java
@@ -24,7 +24,7 @@ public class LearningTraverser {
 			//String filePath = "./glaurung-2.2.cg";
 			//String filePath = "./texel-107.cg";
 			//String filePath = "./stockfish-10.cg";
-			String filePath = "./lc0-v0.25.1.cg";
+			String filePath = "./stockfish-12.cg";
 			
 			PositionsVisitor learning = new LearningVisitorImpl();
 			//PositionsVisitor learning = new EvalDiffVisitorImpl();
