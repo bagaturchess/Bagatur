f69d23cb799f9b8565b7c367a4cb2944df1744cc
Topchiyski <krasimir.topchiyski@sap.com>
2020-12-24 12:44:49 +0200

    Refactoring

--- Sources/BoardScanner/src/bagaturchess/scanner/visitors/ScannerCheckVisitor.java
+++ Sources/BoardScanner/src/bagaturchess/scanner/visitors/ScannerCheckVisitor.java
@@ -23,18 +23,12 @@
 package bagaturchess.scanner.visitors;
 
 
-import java.awt.Color;
-import java.awt.Graphics;
-import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.IOException;
-
-import javax.imageio.ImageIO;
 
 import bagaturchess.bitboard.api.IBitBoard;
 import bagaturchess.bitboard.api.IGameStatus;
-import bagaturchess.bitboard.impl.Constants;
+import bagaturchess.scanner.utils.ImageProperties;
 import bagaturchess.scanner.utils.ScannerUtils;
 import bagaturchess.ucitracker.api.PositionsVisitor;
 import deepnetts.net.ConvolutionalNetwork;
@@ -54,15 +48,7 @@ public class ScannerCheckVisitor implements PositionsVisitor {
 	private double sumDiffs1;
 	private double sumDiffs2;
 	
-	private String PIECES_SET = "set1";
-	
-	private Image[] piecesImages = new Image[13];
-	
-	private int IMAGE_SIZE = 256;
-	private int SQUARE_SIZE = IMAGE_SIZE / 8;
-	
-	private Color BLACK_SQUARE = new Color(120, 120, 120);
-	private Color WHITE_SQUARE = new Color(220, 220, 220);
+	private ImageProperties imageProperties;
 	
 	private static final String NET_FILE = "scanner.bin";
 	private ConvolutionalNetwork network;
@@ -70,8 +56,7 @@ public class ScannerCheckVisitor implements PositionsVisitor {
 	
 	public ScannerCheckVisitor() throws Exception {
 		
-		loadPiecesImages();
-		
+		imageProperties = new ImageProperties();	
 		
 		System.out.println("Loading network ...");
 		
@@ -86,7 +71,7 @@ public class ScannerCheckVisitor implements PositionsVisitor {
 	@Override
 	public void visitPosition(IBitBoard bitboard, IGameStatus status, int expectedWhitePlayerEval) {
         
-		BufferedImage image = createBoardImage(bitboard.toEPD());
+		BufferedImage image = ScannerUtils.createBoardImage(imageProperties, bitboard.toEPD());
 		image = ScannerUtils.convertToGrayScale(image);
 		//ScannerUtils.saveImage(bitboard.toEPD(), image);
 		float[] expected_input = ScannerUtils.convertToFlatGrayArray(image);
@@ -151,119 +136,5 @@ public class ScannerCheckVisitor implements PositionsVisitor {
 	public void end() {
 		System.out.println("END Iteration " + iteration + ": Time " + (System.currentTimeMillis() - startTime) + "ms, " + "Success: " + (100 * (1 - (sumDiffs2 / sumDiffs1))) + "%");
 		//network.save(NET_FILE);
-	}
-	
-	
-	private BufferedImage createBoardImage(String fen) {
-		
-		BufferedImage image = new BufferedImage(IMAGE_SIZE, IMAGE_SIZE, BufferedImage.TYPE_INT_RGB);
-		
-		Graphics g = image.createGraphics();
-		
-		for (int i = 0; i < 8; i++) {
-			for (int j = 0; j < 8; j++) {
-				
-				if ((i + j) % 2 == 0) {
-					g.setColor(WHITE_SQUARE);
-				} else {
-					g.setColor(BLACK_SQUARE);
-				}
-				
-				g.fillRect(i * SQUARE_SIZE, j * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE);
-			}
-		}
-		
-		String[] fenArray = fen.split(" ");
-		int positionCount = 63;
-		for (int i = 0; i < fenArray[0].length(); i++) {
-
-			int x = (7 - positionCount % 8) * SQUARE_SIZE;
-			int y = (7 - positionCount / 8) * SQUARE_SIZE;
-			boolean whiteSquare = (7 - positionCount % 8 + 7 - positionCount / 8) % 2 == 0;
-					
-			final char character = fenArray[0].charAt(i);
-			switch (character) {
-			case '/':
-				continue;
-			case '1':
-			case '2':
-			case '3':
-			case '4':
-			case '5':
-			case '6':
-			case '7':
-			case '8':
-				positionCount -= Character.digit(character, 10);
-				break;
-			case 'P':
-				g.drawImage(piecesImages[Constants.PID_W_PAWN], x, y, SQUARE_SIZE, SQUARE_SIZE, whiteSquare ? WHITE_SQUARE : BLACK_SQUARE, null);
-				positionCount--;
-				break;
-			case 'N':
-				g.drawImage(piecesImages[Constants.PID_W_KNIGHT], x, y, SQUARE_SIZE, SQUARE_SIZE, whiteSquare ? WHITE_SQUARE : BLACK_SQUARE, null);
-				positionCount--;
-				break;
-			case 'B':
-				g.drawImage(piecesImages[Constants.PID_W_BISHOP], x, y, SQUARE_SIZE, SQUARE_SIZE, whiteSquare ? WHITE_SQUARE : BLACK_SQUARE, null);
-				positionCount--;
-				break;
-			case 'R':
-				g.drawImage(piecesImages[Constants.PID_W_ROOK], x, y, SQUARE_SIZE, SQUARE_SIZE, whiteSquare ? WHITE_SQUARE : BLACK_SQUARE, null);
-				positionCount--;
-				break;
-			case 'Q':
-				g.drawImage(piecesImages[Constants.PID_W_QUEEN], x, y, SQUARE_SIZE, SQUARE_SIZE, whiteSquare ? WHITE_SQUARE : BLACK_SQUARE, null);
-				positionCount--;
-				break;
-			case 'K':
-				g.drawImage(piecesImages[Constants.PID_W_KING], x, y, SQUARE_SIZE, SQUARE_SIZE, whiteSquare ? WHITE_SQUARE : BLACK_SQUARE, null);
-				positionCount--;
-				break;
-			case 'p':
-				g.drawImage(piecesImages[Constants.PID_B_PAWN], x, y, SQUARE_SIZE, SQUARE_SIZE, whiteSquare ? WHITE_SQUARE : BLACK_SQUARE, null);
-				positionCount--;
-				break;
-			case 'n':
-				g.drawImage(piecesImages[Constants.PID_B_KNIGHT], x, y, SQUARE_SIZE, SQUARE_SIZE, whiteSquare ? WHITE_SQUARE : BLACK_SQUARE, null);
-				positionCount--;
-				break;
-			case 'b':
-				g.drawImage(piecesImages[Constants.PID_B_BISHOP], x, y, SQUARE_SIZE, SQUARE_SIZE, whiteSquare ? WHITE_SQUARE : BLACK_SQUARE, null);
-				positionCount--;
-				break;
-			case 'r':
-				g.drawImage(piecesImages[Constants.PID_B_ROOK], x, y, SQUARE_SIZE, SQUARE_SIZE, whiteSquare ? WHITE_SQUARE : BLACK_SQUARE, null);
-				positionCount--;
-				break;
-			case 'q':
-				g.drawImage(piecesImages[Constants.PID_B_QUEEN], x, y, SQUARE_SIZE, SQUARE_SIZE, whiteSquare ? WHITE_SQUARE : BLACK_SQUARE, null);
-				positionCount--;
-				break;
-			case 'k':
-				g.drawImage(piecesImages[Constants.PID_B_KING], x, y, SQUARE_SIZE, SQUARE_SIZE, whiteSquare ? WHITE_SQUARE : BLACK_SQUARE, null);
-				positionCount--;
-				break;
-			}
-		}
-		
-		return image;
-	}
-	
-	
-	private void loadPiecesImages() throws IOException{
-		
-		piecesImages[Constants.PID_W_KING] = ImageIO.read(new File("./res/" + PIECES_SET + "_w_k.png")).getScaledInstance(SQUARE_SIZE, SQUARE_SIZE, Image.SCALE_REPLICATE);
-		piecesImages[Constants.PID_W_QUEEN] = ImageIO.read(new File("./res/" + PIECES_SET + "_w_q.png")).getScaledInstance(SQUARE_SIZE, SQUARE_SIZE, Image.SCALE_REPLICATE);
-		piecesImages[Constants.PID_W_ROOK] = ImageIO.read(new File("./res/" + PIECES_SET + "_w_r.png")).getScaledInstance(SQUARE_SIZE, SQUARE_SIZE, Image.SCALE_REPLICATE);
-		piecesImages[Constants.PID_W_BISHOP] = ImageIO.read(new File("./res/" + PIECES_SET + "_w_b.png")).getScaledInstance(SQUARE_SIZE, SQUARE_SIZE, Image.SCALE_REPLICATE);
-		piecesImages[Constants.PID_W_KNIGHT] = ImageIO.read(new File("./res/" + PIECES_SET + "_w_n.png")).getScaledInstance(SQUARE_SIZE, SQUARE_SIZE, Image.SCALE_REPLICATE);
-		piecesImages[Constants.PID_W_PAWN] = ImageIO.read(new File("./res/" + PIECES_SET + "_w_p.png")).getScaledInstance(SQUARE_SIZE, SQUARE_SIZE, Image.SCALE_REPLICATE);
-		
-		piecesImages[Constants.PID_B_KING] = ImageIO.read(new File("./res/" + PIECES_SET + "_b_k.png")).getScaledInstance(SQUARE_SIZE, SQUARE_SIZE, Image.SCALE_REPLICATE);
-		piecesImages[Constants.PID_B_QUEEN] = ImageIO.read(new File("./res/" + PIECES_SET + "_b_q.png")).getScaledInstance(SQUARE_SIZE, SQUARE_SIZE, Image.SCALE_REPLICATE);
-		piecesImages[Constants.PID_B_ROOK] = ImageIO.read(new File("./res/" + PIECES_SET + "_b_r.png")).getScaledInstance(SQUARE_SIZE, SQUARE_SIZE, Image.SCALE_REPLICATE);
-		piecesImages[Constants.PID_B_BISHOP] = ImageIO.read(new File("./res/" + PIECES_SET + "_b_b.png")).getScaledInstance(SQUARE_SIZE, SQUARE_SIZE, Image.SCALE_REPLICATE);
-		piecesImages[Constants.PID_B_KNIGHT] = ImageIO.read(new File("./res/" + PIECES_SET + "_b_n.png")).getScaledInstance(SQUARE_SIZE, SQUARE_SIZE, Image.SCALE_REPLICATE);
-		piecesImages[Constants.PID_B_PAWN] = ImageIO.read(new File("./res/" + PIECES_SET + "_b_p.png")).getScaledInstance(SQUARE_SIZE, SQUARE_SIZE, Image.SCALE_REPLICATE);
 	}
 }
