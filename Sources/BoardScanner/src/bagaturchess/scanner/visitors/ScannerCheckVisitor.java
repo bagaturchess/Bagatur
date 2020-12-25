159e2f562f13ea109cc0196a2632470ad48f7311
Topchiyski <krasimir.topchiyski@sap.com>
2020-12-25 18:01:31 +0200

    Board checker implementation

--- Sources/BoardScanner/src/bagaturchess/scanner/visitors/ScannerCheckVisitor.java
+++ Sources/BoardScanner/src/bagaturchess/scanner/visitors/ScannerCheckVisitor.java
@@ -24,16 +24,13 @@ package bagaturchess.scanner.visitors;
 
 
 import java.awt.image.BufferedImage;
-import java.io.File;
 
 import bagaturchess.bitboard.api.IBitBoard;
 import bagaturchess.bitboard.api.IGameStatus;
+import bagaturchess.scanner.utils.BoardScanner;
 import bagaturchess.scanner.utils.ImageProperties;
 import bagaturchess.scanner.utils.ScannerUtils;
 import bagaturchess.ucitracker.api.PositionsVisitor;
-import deepnetts.net.ConvolutionalNetwork;
-import deepnetts.util.FileIO;
-import deepnetts.util.Tensor;
 
 
 public class ScannerCheckVisitor implements PositionsVisitor {
@@ -50,21 +47,12 @@ public class ScannerCheckVisitor implements PositionsVisitor {
 	
 	private ImageProperties imageProperties;
 	
-	private static final String NET_FILE = "scanner.bin";
-	private ConvolutionalNetwork network;
+	private BoardScanner scanner;
 	
 	
 	public ScannerCheckVisitor() throws Exception {
-		
-		imageProperties = new ImageProperties();	
-		
-		System.out.println("Loading network ...");
-		
-		
-		network = (ConvolutionalNetwork) FileIO.createFromFile(new File(NET_FILE));
-		
-		
-		System.out.println("Network loaded.");
+		imageProperties = new ImageProperties(192);
+		scanner = new BoardScanner();
 	}
 	
 	
@@ -73,49 +61,23 @@ public class ScannerCheckVisitor implements PositionsVisitor {
         
 		BufferedImage image = ScannerUtils.createBoardImage(imageProperties, bitboard.toEPD());
 		image = ScannerUtils.convertToGrayScale(image);
+		
 		//ScannerUtils.saveImage(bitboard.toEPD(), image);
 		float[] expected_input = ScannerUtils.convertToFlatGrayArray(image);
-		float[] expected_output = ScannerUtils.createOutputArray(bitboard);
+		String recognized_fen = scanner.scan(expected_input);
 		
-		network.setInput(new Tensor(expected_input));
-		network.forward();
-		float[] actual_output = network.getOutput();
+		String expected_fen_prefix = bitboard.toEPD().split(" ")[0];
 		
-		String fen = ScannerUtils.convertOutputToFEN(actual_output);
-		System.out.println(fen + " < " + bitboard.toEPD());
-		
-		/*String msg = "";
-		for (int i = 0; i < actual_output.length; i++) {
-			msg += actual_output[i] + ", ";
+		sumDiffs1++;
+		if (!recognized_fen.equals(expected_fen_prefix)) {
+			sumDiffs2++;
 		}
-		System.out.println(msg);*/
-		
-		sumDiffs1 += sumExpectedOutput(expected_output);
-		sumDiffs2 += sumDeltaOutput(expected_output, actual_output);
 		
 		counter++;
 		if ((counter % 100) == 0) {
 			
 			System.out.println("Iteration " + iteration + ": Time " + (System.currentTimeMillis() - startTime) + "ms, " + "Success: " + (100 * (1 - (sumDiffs2 / sumDiffs1))) + "%");
 		}
-	}
-
-
-	private double sumExpectedOutput(float[] expected_output) {
-		double sum = 0;
-		for (int i = 0; i < expected_output.length; i++) {
-			sum += Math.abs(0 - expected_output[i]);
-		}
-		return sum;
-	}
-	
-	
-	private double sumDeltaOutput(float[] expected_output, float[] actual_output) {
-		double sum = 0;
-		for (int i = 0; i < expected_output.length; i++) {
-			sum += Math.abs(expected_output[i] - actual_output[i]);
-		}
-		return sum;
 	}
 	
 	
@@ -127,8 +89,8 @@ public class ScannerCheckVisitor implements PositionsVisitor {
 		counter = 0;
 		iteration++;
 		
-		sumDiffs1 = 1;
-		sumDiffs2 = 1;
+		sumDiffs1 = 0;
+		sumDiffs2 = 0;
 	}
 	
 	
