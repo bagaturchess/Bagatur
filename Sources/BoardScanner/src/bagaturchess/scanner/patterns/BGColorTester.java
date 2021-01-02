30120301da83b366d93ba19176880234ec285d1a
Topchiyski <krasimir.topchiyski@sap.com>
2021-01-02 17:40:46 +0200

    GRAY_COLORS

--- Sources/BoardScanner/src/bagaturchess/scanner/patterns/BGColorTester.java
+++ Sources/BoardScanner/src/bagaturchess/scanner/patterns/BGColorTester.java
@@ -20,6 +20,9 @@
 package bagaturchess.scanner.patterns;
 
 
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.color.ColorSpace;
 import java.awt.image.BufferedImage;
 import java.io.File;
 
@@ -31,11 +34,27 @@ import bagaturchess.scanner.cnn.impl.utils.ScannerUtils;
 public class BGColorTester {
 	
 	
+	private static final Color[] GRAY_COLORS = new Color[256];
+	
+	static {
+		
+		for (int r = 0; r < 256; r++) {
+			for (int g = 0; g < 256; g++) {
+				for (int b = 0; b < 256; b++) {
+					int gray = (int) (r * 0.2989d + g * 0.5870 + b * 0.1140);
+					GRAY_COLORS[gray] = new Color(r, g, b);
+				}
+			}
+		}
+	}
+	
+	
 	public static void main(String[] args) {
 		
 		try {
 			
-			int[][] imageMatrix = ScannerUtils.createSquareImage(137, 64);
+			int[][] imageMatrix = createSquareImage(GRAY_COLORS[200], 64);
+			System.out.println(imageMatrix[0][0]);
 			BufferedImage image = ScannerUtils.createGrayImage(imageMatrix);
 			ScannerUtils.saveImage("source", image, "png");
 			
@@ -70,5 +89,42 @@ public class BGColorTester {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+	
+	
+	public static final int[][] createSquareImage(Color bgcolor, int size) {
+		BufferedImage imageSquare = new BufferedImage(size, size, BufferedImage.TYPE_INT_ARGB);
+		Graphics g = imageSquare.getGraphics();
+		g.setColor(bgcolor);
+		g.fillRect(0, 0, imageSquare.getWidth(), imageSquare.getHeight());
+		//imageSquare = ScannerUtils.convertToGrayScale(imageSquare);
+		return convertToGrayMatrix(imageSquare);
+	}
+	
+	
+	public static int[][] convertToGrayMatrix(BufferedImage image) {
+		
+		if (image.getHeight() != image.getWidth()) {
+			throw new IllegalStateException();
+		}
+		
+		int[][] inputs = new int[image.getHeight()][image.getHeight()];
+		for (int i = 0; i < image.getHeight(); i++) {
+			for (int j = 0; j < image.getHeight(); j++) {
+				
+				int rgb = image.getRGB(i, j);
+				
+				//int alpha = (rgb & 0xff000000) >>> 24;
+				int red = (rgb & 0xff0000) >> 16;
+				int green = (rgb & 0xff00) >> 8;
+				int blue = rgb & 0xff;
+				
+				//inputs[count] = (red + green + blue) / 3;
+			    //inputs[count++] = red * 0.299 + green * 0.587 + blue * 0.114;
+				inputs[i][j] = (int) (red * 0.2989d + green * 0.5870d + blue * 0.1140d);
+			}
+		}
+		
+		return inputs;
 	}
 }
