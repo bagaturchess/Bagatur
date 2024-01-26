32a3ec4503553e10c13032912041b7dc72c97f2f
Krasimir Topchiyski <kitopchiyski@gmail.com>
2024-01-26 14:29:41 +0200

    Use 2/3 from NNUE eval, not 1/3

--- LearningImpl/src/bagaturchess/deeplearning/impl_nnue_v2/eval/NNUEEvaluator.java
+++ LearningImpl/src/bagaturchess/deeplearning/impl_nnue_v2/eval/NNUEEvaluator.java
@@ -8,6 +8,7 @@ import java.io.File;
 
 import bagaturchess.bitboard.api.BoardUtils;
 import bagaturchess.bitboard.api.IBitBoard;
+import bagaturchess.bitboard.api.IBoardConfig;
 import bagaturchess.bitboard.impl.Constants;
 import bagaturchess.nnue.NNUEJNIBridge;
 import bagaturchess.nnue.NNUEProbeUtils;
@@ -17,6 +18,9 @@ import bagaturchess.search.impl.eval.cache.IEvalCache;
 
 
 public class NNUEEvaluator extends BaseEvaluator {
+	
+	
+	private static final int MAX_MATERIAL_FACTOR = 4 * 3 + 4 * 3 + 4 * 5 + 2 * 9;
 	
 	
 	static {
@@ -34,6 +38,8 @@ public class NNUEEvaluator extends BaseEvaluator {
 	
 	private IBitBoard bitboard;
 	
+	private final IBoardConfig board_cfg;
+	
 	private NNUEProbeUtils.Input input;
 	
 	
@@ -42,6 +48,8 @@ public class NNUEEvaluator extends BaseEvaluator {
 		super(_bitboard, _evalCache, _evalConfig);
 		
 		bitboard = _bitboard;
+		
+		board_cfg = bitboard.getBoardConfig();
 		
 		input = new NNUEProbeUtils.Input();
 	}
@@ -57,10 +65,49 @@ public class NNUEEvaluator extends BaseEvaluator {
 	@Override
 	protected int phase1() {
 		
-
+		/*int count_pawns = Long.bitCount(bitboard.getFiguresBitboardByColourAndType(Constants.COLOUR_WHITE, Constants.TYPE_PAWN))
+							- Long.bitCount(bitboard.getFiguresBitboardByColourAndType(Constants.COLOUR_BLACK, Constants.TYPE_PAWN));
+		int count_knights = Long.bitCount(bitboard.getFiguresBitboardByColourAndType(Constants.COLOUR_WHITE, Constants.TYPE_KNIGHT))
+							- Long.bitCount(bitboard.getFiguresBitboardByColourAndType(Constants.COLOUR_BLACK, Constants.TYPE_KNIGHT));
+		int count_bishops = Long.bitCount(bitboard.getFiguresBitboardByColourAndType(Constants.COLOUR_WHITE, Constants.TYPE_BISHOP))
+							- Long.bitCount(bitboard.getFiguresBitboardByColourAndType(Constants.COLOUR_BLACK, Constants.TYPE_BISHOP));
+		int count_rooks = Long.bitCount(bitboard.getFiguresBitboardByColourAndType(Constants.COLOUR_WHITE, Constants.TYPE_ROOK))
+							- Long.bitCount(bitboard.getFiguresBitboardByColourAndType(Constants.COLOUR_BLACK, Constants.TYPE_ROOK));
+		int count_queens = Long.bitCount(bitboard.getFiguresBitboardByColourAndType(Constants.COLOUR_WHITE, Constants.TYPE_QUEEN))
+							- Long.bitCount(bitboard.getFiguresBitboardByColourAndType(Constants.COLOUR_BLACK, Constants.TYPE_QUEEN));
+		
+		int score_o = 0;
+		int score_e = 0;
+		
+		score_o += (int) (count_pawns * board_cfg.getMaterial_PAWN_O());
+		score_o += (int) (count_knights * board_cfg.getMaterial_KNIGHT_O());
+		score_o += (int) (count_bishops * board_cfg.getMaterial_BISHOP_O());
+		score_o += (int) (count_rooks * board_cfg.getMaterial_ROOK_O());
+		score_o += (int) (count_queens * board_cfg.getMaterial_QUEEN_O());
+		
+		score_e += (int) (count_pawns * board_cfg.getMaterial_PAWN_E());
+		score_e += (int) (count_knights * board_cfg.getMaterial_KNIGHT_E());
+		score_e += (int) (count_bishops * board_cfg.getMaterial_BISHOP_E());
+		score_e += (int) (count_rooks * board_cfg.getMaterial_ROOK_E());
+		score_e += (int) (count_queens * board_cfg.getMaterial_QUEEN_E());
+		
+		int total_material_factor = Math.min(MAX_MATERIAL_FACTOR, bitboard.getMaterialFactor().getTotalFactor());
+		
+		return (int) (score_o * total_material_factor + score_e * (MAX_MATERIAL_FACTOR - total_material_factor)) / MAX_MATERIAL_FACTOR;
+		*/
+		
+		return 0;
+	}
+	
+	
+	@Override
+	protected int phase2() {
+		
 		NNUEProbeUtils.fillInput(bitboard, input);
 		
-		int actualWhitePlayerEval = NNUEJNIBridge.eval(input.color, input.pieces, input.squares) / 3;
+		int actualWhitePlayerEval = NNUEJNIBridge.eval(input.color, input.pieces, input.squares);
+		
+		actualWhitePlayerEval = (2 * actualWhitePlayerEval) / 3;
 		
 		//String fen = bitboard.toEPD();
 		
@@ -72,15 +119,6 @@ public class NNUEEvaluator extends BaseEvaluator {
 		}
 		
 		return (int) actualWhitePlayerEval;
-	}
-	
-	
-	@Override
-	protected int phase2() {
-
-		int eval = 0;
-		
-		return eval;
 	}
 	
 	
