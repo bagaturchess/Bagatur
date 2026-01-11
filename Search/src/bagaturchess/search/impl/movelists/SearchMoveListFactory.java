e34b55674cbe0e3a3b3b23d38cd71319ce1c02b5
Krasimir Topchiyski <k_topchiyski@yahoo.com>
2026-01-12 00:18:53 +0200

    Use only MVVLVA in captures sorting

--- Search/src/bagaturchess/search/impl/movelists/SearchMoveListFactory.java
+++ Search/src/bagaturchess/search/impl/movelists/SearchMoveListFactory.java
@@ -33,8 +33,8 @@ public class SearchMoveListFactory implements ISearchMoveListFactory {
 	
 	@Override
 	public IMoveList createListCaptures(SearchEnv env, boolean onTheFlySorting) {
-		//return new SortedMoveList_MVVLVA(333, env, onTheFlySorting);
-		return new SortedMoveList_SEEMVVLVA(333, env, onTheFlySorting);
+		return new SortedMoveList_MVVLVA(333, env, onTheFlySorting);
+		//return new SortedMoveList_SEEMVVLVA(333, env, onTheFlySorting);
 		//return new SortedMoveList_CaptureHistory(333, env, onTheFlySorting);
 	}
 }
