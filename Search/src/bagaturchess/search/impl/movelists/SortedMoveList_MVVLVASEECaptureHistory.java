6031e8984fa3c86d38e7211499e9892e5f4f1be2
Krasimir Topchiyski <k_topchiyski@yahoo.com>
2026-01-17 21:15:06 +0200

    Combined ordering in qsearch

---
+++ Search/src/bagaturchess/search/impl/movelists/SortedMoveList_MVVLVASEECaptureHistory.java
@@ -0,0 +1,48 @@
+/*
+ *  BagaturChess (UCI chess engine and tools)
+ *  Copyright (C) 2005 Krasimir I. Topchiyski (k_topchiyski@yahoo.com)
+ *  
+ *  Open Source project location: http://sourceforge.net/projects/bagaturchess/develop
+ *  SVN repository https://bagaturchess.svn.sourceforge.net/svnroot/bagaturchess
+ *
+ *  This file is part of BagaturChess program.
+ * 
+ *  BagaturChess is open software: you can redistribute it and/or modify
+ *  it under the terms of the Eclipse Public License version 1.0 as published by
+ *  the Eclipse Foundation.
+ *
+ *  BagaturChess is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  Eclipse Public License for more details.
+ *
+ *  You should have received a copy of the Eclipse Public License version 1.0
+ *  along with BagaturChess. If not, see <http://www.eclipse.org/legal/epl-v10.html/>.
+ *
+ */
+package bagaturchess.search.impl.movelists;
+
+
+import bagaturchess.bitboard.impl2.MoveUtil;
+import bagaturchess.search.impl.env.SearchEnv;
+
+
+public class SortedMoveList_MVVLVASEECaptureHistory extends SortedMoveList_BaseImpl {
+	
+	
+	public SortedMoveList_MVVLVASEECaptureHistory(int max, SearchEnv _env, boolean onTheFlySorting) {
+		super(max, _env, onTheFlySorting);
+	}
+	
+	
+	@Override
+	protected int getOrderingValue(int move) {
+		
+		int score = 10000 * MoveUtil.getAttackedPieceIndex(move)
+			      - 1000  * MoveUtil.getSourcePieceIndex(move)
+			      + 4     * env.getBitboard().getSEEScore(move)
+			      + env.getCaptureHistory().getScores(-1, move);
+		
+		return score;
+	}
+}
