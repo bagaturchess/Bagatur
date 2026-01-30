7a7adb608aaffca8600c42032ccf90cca0c0f549
Krasimir Topchiyski <k_topchiyski@yahoo.com>
2026-01-31 01:38:41 +0200

    Use captures history for sorting captures in search

--- Search/src/bagaturchess/search/impl/movelists/SearchMoveListFactory.java
+++ Search/src/bagaturchess/search/impl/movelists/SearchMoveListFactory.java
@@ -37,7 +37,7 @@ public class SearchMoveListFactory implements ISearchMoveListFactory {
 	@Override
 	public IMoveList createListCaptures(SearchEnv env, boolean onTheFlySorting) {
 		
-		return new SortedMoveList_SEEMVVLVA(333, env, onTheFlySorting);
+		return new SortedMoveList_CaptureHistory(333, env, onTheFlySorting);
 	}
 
 
